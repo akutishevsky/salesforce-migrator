61bb48134759af6083e3b4053d15b16db8d85e55
Anton Kutishevsky <akutishevsky@gmail.com>
2025-03-30 16:08:39 +0000

    feat: utilized the HtmlService class for content rendering

--- src/OrgSelector.ts
+++ src/OrgSelector.ts
@@ -1,9 +1,11 @@
 import * as vscode from "vscode";
 import { SfCommandService } from "./SfCommandService";
+import { HtmlService } from "./HtmlService";
 
 export class OrgSelector implements vscode.WebviewViewProvider {
     private _extensionContext: vscode.ExtensionContext;
     private _sfCommandService: SfCommandService;
+    private _htmlService!: HtmlService;
 
     constructor(extensionContext: vscode.ExtensionContext) {
         this._extensionContext = extensionContext;
@@ -15,17 +17,29 @@ export class OrgSelector implements vscode.WebviewViewProvider {
         context: vscode.WebviewViewResolveContext,
         token: vscode.CancellationToken
     ): Promise<void> {
+        this._htmlService = new HtmlService({
+            view: webviewView,
+            extensionUri: this._extensionContext.extensionUri,
+        });
+
         webviewView.webview.options = {
             enableScripts: true,
             localResourceRoots: [this._extensionContext.extensionUri],
         };
-        const orgs = await this._sfCommandService.execute("sf org list");
 
+        const orgs = await this._sfCommandService.execute("sf org list");
         if (orgs.length === 0) {
             webviewView.webview.html = "<p>No orgs found</p>";
             return;
         }
 
+        webviewView.webview.html = this._htmlService.composeHtml({
+            body: this._composeOrgsHtml(orgs),
+            scripts: ["/resources/js/orgSelector.js"],
+        });
+    }
+
+    private _composeOrgsHtml(orgs: any): string {
         let html = `<div>`;
         for (const orgCategory of Object.keys(orgs)) {
             if (orgs[orgCategory].length === 0) {
@@ -45,8 +59,6 @@ export class OrgSelector implements vscode.WebviewViewProvider {
         }
         html += "</div>";
 
-        webviewView.webview.html = html;
-
-        return;
+        return html;
     }
 }
