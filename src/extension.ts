9bb204f726276a311d275f71cb8663580b5a228a
Anton Kutishevsky <akutishevsky@gmail.com>
2025-03-30 14:59:41 +0000

    feat: added target org selector to extension activation

--- src/extension.ts
+++ src/extension.ts
@@ -4,11 +4,16 @@ import { OrgSelector } from "./OrgSelector";
 export function activate(extensionContext: vscode.ExtensionContext) {
     try {
         const sourceOrgSelector = new OrgSelector(extensionContext);
+        const targetOrgSelector = new OrgSelector(extensionContext);
 
         extensionContext.subscriptions.push(
             vscode.window.registerWebviewViewProvider(
                 "salesforce-migrator.source-org-selector",
                 sourceOrgSelector
+            ),
+            vscode.window.registerWebviewViewProvider(
+                "salesforce-migrator.target-org-selector",
+                targetOrgSelector
             )
         );
     } catch (error) {
