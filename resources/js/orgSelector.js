8e966202f090720af0890885e0d6027f399b6aa9
Anton Kutishevsky <akutishevsky@gmail.com>
2025-03-30 16:26:11 +0000

    refact: extracted org selection logic into a separate function

--- resources/js/orgSelector.js
+++ resources/js/orgSelector.js
@@ -1,22 +1,26 @@
 const vscode = acquireVsCodeApi();
 
+function selectSalesforceOrg(event, radios) {
+    const selectedOrgAlias = event.target.value;
+
+    radios.forEach((r) => {
+        if (r !== event.target) {
+            r.checked = false;
+        }
+    });
+
+    vscode.postMessage({
+        action: "orgSelected",
+        orgAlias: selectedOrgAlias,
+    });
+}
+
 (() => {
     window.addEventListener("load", (event) => {
         const radios = document.querySelectorAll("input[type='radio']");
         radios.forEach((radio) => {
             radio.addEventListener("change", (event) => {
-                const selectedOrgAlias = event.target.value;
-
-                radios.forEach((r) => {
-                    if (r !== event.target) {
-                        r.checked = false;
-                    }
-                });
-
-                vscode.postMessage({
-                    action: "orgSelected",
-                    orgAlias: selectedOrgAlias,
-                });
+                selectSalesforceOrg(event, radios);
             });
         });
     });
