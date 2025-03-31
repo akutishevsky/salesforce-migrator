ae7b64824388e07bf674ff3a74c1c99d6cf03471
Anton Kutishevsky <akutishevsky@gmail.com>
2025-03-31 10:48:17 +0000

    feat: added functionality to render metadata of the selected type

--- src/webviews/MetadataDeploymentWebview.ts
+++ src/webviews/MetadataDeploymentWebview.ts
@@ -1,11 +1,15 @@
 import * as vscode from "vscode";
 import { HtmlService } from "../services/HtmlService";
+import { OrgService } from "../services/OrgService";
+import { SfCommandService } from "../services/SfCommandService";
 
 export class MetadataDeploymentWebview {
     private _extensionContext: vscode.ExtensionContext;
     private _webviewView: vscode.WebviewView;
     private _htmlService: HtmlService;
     private _panel: vscode.WebviewPanel | undefined;
+    private _orgService!: OrgService;
+    private _sfCommandService!: SfCommandService;
 
     constructor(
         extensionContext: vscode.ExtensionContext,
@@ -17,36 +21,118 @@ export class MetadataDeploymentWebview {
             view: webviewView,
             extensionUri: this._extensionContext.extensionUri,
         });
+        this._orgService = new OrgService(extensionContext);
+        this._sfCommandService = new SfCommandService();
     }
 
-    public reveal(metadata?: string): void {
-        const webviewTitle = `${metadata} Deployment`;
+    public async reveal(metadataType?: string): Promise<void> {
+        try {
+            const webviewTitle = `${metadataType} Deployment`;
 
-        if (!this._panel) {
-            this._panel = vscode.window.createWebviewPanel(
-                "salesforce-migrator.metadata-deployment",
-                webviewTitle,
-                vscode.ViewColumn.One,
-                {
-                    enableScripts: true,
-                    localResourceRoots: [this._extensionContext.extensionUri],
-                }
+            if (!this._panel) {
+                this._panel = vscode.window.createWebviewPanel(
+                    "salesforce-migrator.metadata-deployment",
+                    webviewTitle,
+                    vscode.ViewColumn.One,
+                    {
+                        enableScripts: true,
+                        localResourceRoots: [
+                            this._extensionContext.extensionUri,
+                        ],
+                    }
+                );
+
+                this._panel.onDidDispose(() => {
+                    this._panel = undefined;
+                });
+            }
+
+            this._panel.title = webviewTitle;
+
+            const sourceOrg = this._orgService.getSourceOrg();
+            if (!sourceOrg) {
+                vscode.window.showErrorMessage(
+                    "No source org selected. Please select a source org first."
+                );
+                return;
+            }
+
+            const targetOrg = this._orgService.getTargetOrg();
+            if (!targetOrg) {
+                vscode.window.showErrorMessage(
+                    "No target org selected. Please select a target org first."
+                );
+                return;
+            }
+
+            const metadata = await this._sfCommandService.execute(
+                `sf org list metadata --target-org ${sourceOrg} --metadata-type ${metadataType}`
             );
 
-            this._panel.onDidDispose(() => {
-                this._panel = undefined;
+            this._panel.webview.html = this._htmlService.composeHtml({
+                body: this._composeWebviewHtml(metadata),
             });
+
+            this._panel.reveal();
+        } catch (error) {
+            vscode.window.showErrorMessage(
+                `Error while deploying metadata: ${error}`
+            );
         }
+    }
 
-        this._panel.title = webviewTitle;
-        const content = metadata
-            ? `Selected metadata: ${metadata}`
-            : "Hello, Metadata Deployment!";
+    private _composeWebviewHtml(metadata: any): string {
+        let html = "";
 
-        this._panel.webview.html = this._htmlService.composeHtml({
-            body: content,
+        html += `
+            <table>
+                <thead>
+                    ${this._composeTableHeadHtml(metadata)}
+                </thead>
+                <tbody>
+                    ${this._composeTableBodyHtml(metadata)}
+                </tbody>
+            </table>
+        `;
+
+        return html;
+    }
+
+    private _composeTableHeadHtml(metadata: any): string {
+        return `
+            <tr>
+                <th>Action</th>
+                <th>Full Name</th>
+                <th>Created By</th>
+                <th>Modified By</th>
+                <th>Created Date</th>
+                <th>Modified Date</th>
+            </tr>
+        `;
+    }
+
+    private _composeTableBodyHtml(metadata: any): string {
+        let html = "";
+
+        metadata.sort((a: any, b: any) => {
+            return a.fullName.localeCompare(b.fullName);
         });
 
-        this._panel.reveal();
+        for (const item of metadata) {
+            html += `
+                <tr>
+                    <td>
+                        <button>Retrieve</button>
+                        <button>Deploy</button>
+                    </td>
+                    <td>${item.fullName}</td>
+                    <td>${item.createdByName}</td>
+                    <td>${item.lastModifiedByName}</td>
+                    <td>${new Date(item.createdDate).toLocaleString()}</td>
+                    <td>${new Date(item.lastModifiedDate).toLocaleString()}</td>
+                </tr>`;
+        }
+
+        return html;
     }
 }
