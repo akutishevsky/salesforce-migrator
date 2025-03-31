fdf912fc2bb7dd75dfe9211fe3a3fc4f58984c00
Anton Kutishevsky <akutishevsky@gmail.com>
2025-03-31 18:37:26 +0000

    feat: inialized basic variables

--- src/views/RecordsSelectorView.ts
+++ src/views/RecordsSelectorView.ts
@@ -1,7 +1,10 @@
 import * as vscode from "vscode";
+import { HtmlService } from "../services/HtmlService";
 
 export class RecordsSelectorView implements vscode.WebviewViewProvider {
     private _extensionContext: vscode.ExtensionContext;
+    private _webviewView: vscode.WebviewView | undefined;
+    private _htmlService!: HtmlService;
 
     constructor(extensionContext: vscode.ExtensionContext) {
         this._extensionContext = extensionContext;
@@ -11,5 +14,16 @@ export class RecordsSelectorView implements vscode.WebviewViewProvider {
         webviewView: vscode.WebviewView,
         context: vscode.WebviewViewResolveContext,
         token: vscode.CancellationToken
-    ): Promise<void> {}
+    ): Promise<void> {
+        this._webviewView = webviewView;
+        this._htmlService = new HtmlService({
+            view: this._webviewView,
+            extensionUri: this._extensionContext.extensionUri,
+        });
+
+        webviewView.webview.options = {
+            enableScripts: true,
+            localResourceRoots: [this._extensionContext.extensionUri],
+        };
+    }
 }
