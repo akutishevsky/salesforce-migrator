5b4820091a8f84cab6154f7fa364870b6a145a42
Anton Kutishevsky <akutishevsky@gmail.com>
2025-03-31 06:23:28 +0000

    feat: added functionality for fetching metadata

--- src/webviews/MetadataSelectorWebview.ts
+++ src/webviews/MetadataSelectorWebview.ts
@@ -1,13 +1,19 @@
 import * as vscode from "vscode";
 import { HtmlService } from "../services/HtmlService";
+import { SfCommandService } from "../services/SfCommandService";
+import { OrgService } from "../services/OrgService";
 
 export class MetadataSelectorWebview implements vscode.WebviewViewProvider {
     private _extensionContext: vscode.ExtensionContext;
     private _htmlService!: HtmlService;
     private _webviewView: vscode.WebviewView | undefined;
+    private _sfCommandService: SfCommandService;
+    private _orgService: OrgService;
 
     constructor(extensionContext: vscode.ExtensionContext) {
         this._extensionContext = extensionContext;
+        this._sfCommandService = new SfCommandService();
+        this._orgService = new OrgService(extensionContext);
     }
 
     public async resolveWebviewView(
@@ -25,5 +31,20 @@ export class MetadataSelectorWebview implements vscode.WebviewViewProvider {
             enableScripts: true,
             localResourceRoots: [this._extensionContext.extensionUri],
         };
+
+        const sourceOrg = this._orgService.getSourceOrg();
+        if (!sourceOrg) {
+            vscode.window.showErrorMessage(
+                "No source org selected. Please select a source org first."
+            );
+            return;
+        }
+
+        const metadata = await this._sfCommandService.execute(
+            `sf org list metadata-types --target-org ${sourceOrg}`
+        );
+        const metadataObjects = metadata.metadataObjects;
+
+        console.log(metadataObjects);
     }
 }
