3ea80ef06c09cb8717827828f823a3c65bf2e3c3
Anton Kutishevsky <akutishevsky@gmail.com>
2025-04-01 17:50:02 +0000

    feat: added filtering functionality

--- resources/js/recordsMigrationExport.js
+++ resources/js/recordsMigrationExport.js
@@ -17,14 +17,44 @@ const updateQuery = () => {
     )}\nFROM ${objectName}`;
 };
 
+const filterFields = () => {
+    const filterInput = document.querySelector(".sfm-filter > input");
+    filterInput.addEventListener("input", (e) => {
+        const filterValue = e.target.value.toLowerCase();
+        const fieldItems = document.querySelectorAll(".sfm-field-item");
+        fieldItems.forEach((item) => {
+            const fieldName = item
+                .querySelector(".sfm-field-label-name")
+                .textContent.toLowerCase();
+            const fieldApiName = item
+                .querySelector(".sfm-field-api-name")
+                .textContent.toLowerCase();
+            const fieldType = item
+                .querySelector(".sfm-field-type")
+                .textContent.toLowerCase();
+
+            const isMatching =
+                fieldName.includes(filterValue) ||
+                fieldApiName.includes(filterValue) ||
+                fieldType.includes(filterValue);
+
+            if (isMatching) {
+                item.style.display = "block";
+            } else {
+                item.style.display = "none";
+            }
+        });
+    });
+};
+
 const initPage = () => {
     const fieldCheckboxes = document.querySelectorAll(
         ".sfm-field-item > input[type='checkbox']"
     );
-
     fieldCheckboxes.forEach((checkbox) => {
         checkbox.addEventListener("change", updateQuery);
     });
+    filterFields();
 };
 
 window.addEventListener("load", initPage);
