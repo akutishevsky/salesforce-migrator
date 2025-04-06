8efcc799614f6bbc589d33b1760235546662c76e
Anton Kutishevsky <akutishevsky@gmail.com>
2025-04-06 05:14:09 +0000

    feat: implemented reveal method and panel initialization in RecordsMigrationDml

--- src/webviews/RecordsMigrationDml.ts
+++ src/webviews/RecordsMigrationDml.ts
@@ -1,10 +1,13 @@
 import * as vscode from "vscode";
+import { HtmlService } from "../services/HtmlService";
 
 export class RecordsMigrationDml {
     private _extensionContext: vscode.ExtensionContext;
     private _webviewView: vscode.WebviewView;
     private _customObject: string;
     private _operation: string;
+    private _htmlService: HtmlService;
+    private _panel: vscode.WebviewPanel | undefined;
 
     constructor(
         extensionContext: vscode.ExtensionContext,
@@ -16,7 +19,39 @@ export class RecordsMigrationDml {
         this._webviewView = webviewView;
         this._customObject = customObject;
         this._operation = operation;
+        this._htmlService = new HtmlService({
+            view: webviewView,
+            extensionUri: extensionContext.extensionUri,
+        });
     }
 
-    public async reveal(): Promise<void> {}
+    public async reveal(): Promise<void> {
+        this._initializePanel();
+        this._renderLoader();
+        this._panel!.reveal();
+    }
+
+    private _initializePanel(): void {
+        if (!this._panel) {
+            this._panel = vscode.window.createWebviewPanel(
+                "salesforce-migrator.records-migration-dml",
+                `${this._operation} ${this._customObject} Records`,
+                vscode.ViewColumn.One,
+                {
+                    enableScripts: true,
+                    localResourceRoots: [this._extensionContext.extensionUri],
+                }
+            );
+        }
+    }
+
+    private _renderLoader(): void {
+        if (!this._panel) {
+            vscode.window.showErrorMessage(
+                "Webview panel is not initialized. Please try again."
+            );
+            return;
+        }
+        this._panel!.webview.html = this._htmlService.getLoaderHtml();
+    }
 }
