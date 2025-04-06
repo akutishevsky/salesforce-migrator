b98e68aa4401e91c40efa812da79515aa9064249
Anton Kutishevsky <akutishevsky@gmail.com>
2025-04-06 11:20:47 +0000

    refact: removed try-catch block

--- resources/js/recordsMigrationDml.js
+++ resources/js/recordsMigrationDml.js
@@ -63,49 +63,42 @@ function renderMappingTable(csvHeaders, fields) {
 }
 
 (() => {
-    try {
-        window.addEventListener("load", () => {
-            const sfmContainer = document.querySelector(".sfm-container");
-            dmlOperation = sfmContainer.dataset.dmlOperation;
+    window.addEventListener("load", () => {
+        const sfmContainer = document.querySelector(".sfm-container");
+        dmlOperation = sfmContainer.dataset.dmlOperation;
 
-            const browseFileButton = document.querySelector(
-                "#browse-file-button"
-            );
-            browseFileButton.addEventListener("click", async () => {
-                vscode.postMessage({
-                    command: "selectSourceFile",
-                });
+        const browseFileButton = document.querySelector("#browse-file-button");
+        browseFileButton.addEventListener("click", async () => {
+            vscode.postMessage({
+                command: "selectSourceFile",
             });
         });
+    });
 
-        window.addEventListener("message", (event) => {
-            const message = event.data;
-            switch (message.command) {
-                case "setSourceFile":
-                    document.querySelector("#source-file").value =
-                        message.filePath;
+    window.addEventListener("message", (event) => {
+        const message = event.data;
+        switch (message.command) {
+            case "setSourceFile":
+                document.querySelector("#source-file").value = message.filePath;
 
-                    if (dmlOperation === "Delete") {
-                        return;
-                    }
+                if (dmlOperation === "Delete") {
+                    return;
+                }
 
-                    const sfmMapping = document.querySelector("#sfm-mapping");
-                    sfmMapping.classList.remove("sfm-hidden");
+                const sfmMapping = document.querySelector("#sfm-mapping");
+                sfmMapping.classList.remove("sfm-hidden");
 
-                    const mappingContainer = document.querySelector(
-                        ".sfm-mapping-container"
-                    );
-                    mappingContainer.innerHTML = renderMappingTable(
-                        message.csvHeaders,
-                        message.fields
-                    );
+                const mappingContainer = document.querySelector(
+                    ".sfm-mapping-container"
+                );
+                mappingContainer.innerHTML = renderMappingTable(
+                    message.csvHeaders,
+                    message.fields
+                );
 
-                    break;
-                default:
-                    break;
-            }
-        });
-    } catch (error) {
-        console.error("Error in initializing DML operation:", error);
-    }
+                break;
+            default:
+                break;
+        }
+    });
 })();
