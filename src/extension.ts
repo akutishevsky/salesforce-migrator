94a384e540347399a7c383958f0bd1e2bff82409
Anton Kutishevsky <akutishevsky@gmail.com>
2025-04-09 04:05:32 +0000

    refact: extracted activate function into smaller methods

--- src/extension.ts
+++ src/extension.ts
@@ -5,91 +5,169 @@ import { OrgSelectorWebview } from "./views/OrgSelectorView";
 import { MetadataSelectorView } from "./views/MetadataSelectorView";
 import { RecordsSelectorView } from "./views/RecordsSelectorView";
 
+function checkIsSfdxProject(): boolean {
+    const workspaceFolders = vscode.workspace.workspaceFolders;
+    if (!workspaceFolders || workspaceFolders.length === 0) {
+        vscode.commands.executeCommand(
+            "setContext",
+            "salesforce-migrator.isSfdxProject",
+            false
+        );
+        return false;
+    }
+
+    const projectFilePath = path.join(
+        workspaceFolders[0].uri.fsPath,
+        "sfdx-project.json"
+    );
+    const isSfdxProject = fs.existsSync(projectFilePath);
+    vscode.commands.executeCommand(
+        "setContext",
+        "salesforce-migrator.isSfdxProject",
+        isSfdxProject
+    );
+
+    return isSfdxProject;
+}
+
+function createViewProviders(extensionContext: vscode.ExtensionContext) {
+    const sourceOrgSelector = new OrgSelectorWebview(
+        extensionContext,
+        "source"
+    );
+    const targetOrgSelector = new OrgSelectorWebview(
+        extensionContext,
+        "target"
+    );
+    const metadataSelectorView = new MetadataSelectorView(extensionContext);
+    const recordsSelectorView = new RecordsSelectorView(extensionContext);
+
+    return {
+        sourceOrgSelector,
+        targetOrgSelector,
+        metadataSelectorView,
+        recordsSelectorView,
+    };
+}
+
+function registerCommands(
+    extensionContext: vscode.ExtensionContext,
+    viewProviders: {
+        sourceOrgSelector: OrgSelectorWebview;
+        targetOrgSelector: OrgSelectorWebview;
+        metadataSelectorView: MetadataSelectorView;
+        recordsSelectorView: RecordsSelectorView;
+    }
+) {
+    const {
+        sourceOrgSelector,
+        targetOrgSelector,
+        metadataSelectorView,
+        recordsSelectorView,
+    } = viewProviders;
+
+    const refreshSourceOrgsCommand = vscode.commands.registerCommand(
+        "salesforce-migrator.refreshSourceOrgs",
+        () => sourceOrgSelector.refresh()
+    );
+
+    const refreshTargetOrgsCommand = vscode.commands.registerCommand(
+        "salesforce-migrator.refreshTargetOrgs",
+        () => targetOrgSelector.refresh()
+    );
+
+    const refreshMetadataCommand = vscode.commands.registerCommand(
+        "salesforce-migrator.refreshMetadata",
+        () => metadataSelectorView.refreshMetadata()
+    );
+
+    const refreshRecordsCommand = vscode.commands.registerCommand(
+        "salesforce-migrator.refreshRecords",
+        () => recordsSelectorView.refreshRecords()
+    );
+
+    const clearWorkspaceStorageCommand = vscode.commands.registerCommand(
+        "salesforce-migrator.clearWorkspaceStorage",
+        () => {
+            const orgService = sourceOrgSelector.getOrgService();
+            orgService.clearOrgSelections();
+        }
+    );
+
+    return {
+        refreshSourceOrgsCommand,
+        refreshTargetOrgsCommand,
+        refreshMetadataCommand,
+        refreshRecordsCommand,
+        clearWorkspaceStorageCommand,
+    };
+}
+
+function registerWebviewProviders(
+    extensionContext: vscode.ExtensionContext,
+    viewProviders: {
+        sourceOrgSelector: OrgSelectorWebview;
+        targetOrgSelector: OrgSelectorWebview;
+        metadataSelectorView: MetadataSelectorView;
+        recordsSelectorView: RecordsSelectorView;
+    },
+    commands: {
+        refreshSourceOrgsCommand: vscode.Disposable;
+        refreshTargetOrgsCommand: vscode.Disposable;
+        refreshMetadataCommand: vscode.Disposable;
+        refreshRecordsCommand: vscode.Disposable;
+        clearWorkspaceStorageCommand: vscode.Disposable;
+    }
+) {
+    const {
+        sourceOrgSelector,
+        targetOrgSelector,
+        metadataSelectorView,
+        recordsSelectorView,
+    } = viewProviders;
+    const {
+        refreshSourceOrgsCommand,
+        refreshTargetOrgsCommand,
+        refreshMetadataCommand,
+        refreshRecordsCommand,
+        clearWorkspaceStorageCommand,
+    } = commands;
+
+    extensionContext.subscriptions.push(
+        vscode.window.registerWebviewViewProvider(
+            "salesforce-migrator.source-org-selector",
+            sourceOrgSelector
+        ),
+        vscode.window.registerWebviewViewProvider(
+            "salesforce-migrator.target-org-selector",
+            targetOrgSelector
+        ),
+        vscode.window.registerWebviewViewProvider(
+            "salesforce-migrator.metadata-selector",
+            metadataSelectorView
+        ),
+        vscode.window.registerWebviewViewProvider(
+            "salesforce-migrator.records-selector",
+            recordsSelectorView
+        ),
+
+        refreshSourceOrgsCommand,
+        refreshTargetOrgsCommand,
+        refreshMetadataCommand,
+        refreshRecordsCommand,
+        clearWorkspaceStorageCommand
+    );
+}
+
 export function activate(extensionContext: vscode.ExtensionContext) {
     try {
-        // Check if current workspace is a Salesforce DX project
-        const workspaceFolders = vscode.workspace.workspaceFolders;
-        if (!workspaceFolders || workspaceFolders.length === 0) {
-            // Don't activate if no workspace folder
-            vscode.commands.executeCommand('setContext', 'salesforce-migrator.isSfdxProject', false);
+        if (!checkIsSfdxProject()) {
             return;
         }
 
-        // Check for sfdx-project.json file
-        const projectFilePath = path.join(workspaceFolders[0].uri.fsPath, 'sfdx-project.json');
-        
-        const isSfdxProject = fs.existsSync(projectFilePath);
-        vscode.commands.executeCommand('setContext', 'salesforce-migrator.isSfdxProject', isSfdxProject);
-        
-        // Don't continue if not an SFDX project
-        if (!isSfdxProject) {
-            return;
-        }
-        
-        const sourceOrgSelector = new OrgSelectorWebview(
-            extensionContext,
-            "source"
-        );
-        const targetOrgSelector = new OrgSelectorWebview(
-            extensionContext,
-            "target"
-        );
-        const metadataSelectorView = new MetadataSelectorView(extensionContext);
-        const recordsSelectorView = new RecordsSelectorView(extensionContext);
-
-        // Register commands for refreshing orgs
-        const refreshSourceOrgsCommand = vscode.commands.registerCommand(
-            "salesforce-migrator.refreshSourceOrgs",
-            () => sourceOrgSelector.refresh()
-        );
-
-        const refreshTargetOrgsCommand = vscode.commands.registerCommand(
-            "salesforce-migrator.refreshTargetOrgs",
-            () => targetOrgSelector.refresh()
-        );
-
-        const refreshMetadataCommand = vscode.commands.registerCommand(
-            "salesforce-migrator.refreshMetadata",
-            () => metadataSelectorView.refreshMetadata()
-        );
-
-        const refreshRecordsCommand = vscode.commands.registerCommand(
-            "salesforce-migrator.refreshRecords",
-            () => recordsSelectorView.refreshRecords()
-        );
-
-        const clearWorkspaceStorageCommand = vscode.commands.registerCommand(
-            "salesforce-migrator.clearWorkspaceStorage",
-            () => {
-                const orgService = sourceOrgSelector.getOrgService();
-                orgService.clearOrgSelections();
-            }
-        );
-
-        extensionContext.subscriptions.push(
-            vscode.window.registerWebviewViewProvider(
-                "salesforce-migrator.source-org-selector",
-                sourceOrgSelector
-            ),
-            vscode.window.registerWebviewViewProvider(
-                "salesforce-migrator.target-org-selector",
-                targetOrgSelector
-            ),
-            vscode.window.registerWebviewViewProvider(
-                "salesforce-migrator.metadata-selector",
-                metadataSelectorView
-            ),
-            vscode.window.registerWebviewViewProvider(
-                "salesforce-migrator.records-selector",
-                recordsSelectorView
-            ),
-
-            refreshSourceOrgsCommand,
-            refreshTargetOrgsCommand,
-            refreshMetadataCommand,
-            refreshRecordsCommand,
-            clearWorkspaceStorageCommand
-        );
+        const viewProviders = createViewProviders(extensionContext);
+        const commands = registerCommands(extensionContext, viewProviders);
+        registerWebviewProviders(extensionContext, viewProviders, commands);
     } catch (error) {
         vscode.window.showErrorMessage(
             `Failed to activate the extension: ${error}`
